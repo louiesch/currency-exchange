5a1c64a0bc62042d317e69d110c036d020aeb31e
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-12 14:53:47 -0800

    finish UI logic

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyConversion from './xchange.js'
+import CurrencyConversion from './xchange-service.js';
 
 function clearFields() {
   $('#dollars').val('');
@@ -12,5 +12,26 @@ function clearFields() {
   $('.showConversion').text('');
 }
 
+
+function showExchange(response, USD, currency) {
+  if (response.main) {
+    $('.showConversion').text(`${response.main}%`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
 // UI LOGIC ------------------------
 
+$(document).ready(function() {
+  $('#convertDollar').click(function(event) {
+    event.preventDefault();
+    
+    let USD = $('#dollars').val();
+    let currency = $('input:radio:checked').val();
+    CurrencyConversion.showConversion()
+      .then(function(response) {
+        showExchange(response, USD, currency);
+      });
+  });
+});
