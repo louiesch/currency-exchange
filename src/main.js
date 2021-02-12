001d93a22897c32eed044eca35419d1aee1a5dc7
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-12 14:16:36 -0800

    add output field for errors in html, xchange logic and main.js business logic

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyConversion from './xchange.js'
 
+function clearFields() {
+  $('#dollars').val('');
+  $('.showErrors').text('');
+  $('.showConversion').text('');
+}
 
 // UI LOGIC ------------------------
+
